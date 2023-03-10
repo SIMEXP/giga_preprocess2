35257d503ad11e20db9b52a3a4678befbc5205d9
Natasha Clarke <57987005+clarkenj@users.noreply.github.com>
2023-03-10 09:52:58 -0500

    NC added suggested code to detect_timeout.py

--- scripts/detect_timeout.py
+++ scripts/detect_timeout.py
@@ -1,6 +1,44 @@
 import argparse
 import re
+from nltk import word_tokenize
 from pathlib import Path
+
+#path to directory containing error logs as .txt files
+error_p = Path("__file__").resolve().parents[0] / 'adni_error_logs'
+
+log_list = []
+out_memory_list = []
+time_limit_list = []
+trait_list = []
+
+for file in error_p.iterdir(): #for every file in the path
+    log_list.append(file) #save the file's name in the list_files list
+    with open(file) as f:
+        f_lines = f.readlines() #reads over each line of the file
+
+    for line in f_lines: #for each line of the file
+        split_line = line.split() #splits the contents of each line according to white space.
+
+        #loop through tokens and see if they correspond to common error terms. If so, add file name to list
+        for token in split_line:
+            if token == 'out-of-memory':
+                out_memory_list.append(file.stem)
+
+            if token == 'LIMIT': 
+                time_limit_list.append(file.stem)
+
+            if token == 'FunctionalSummaryInputSpec':
+                trait_list.append(file.stem)
+
+print ('{} error logs'.format(len(log_list)))
+print ('{} may have been killed by the out-of-memory handler'.format(len(out_memory_list)))
+print ('{} were cancelled due to time limit'.format(len(time_limit_list)))
+print ('{} have a FunctionalSummaryInputSpec error'.format(len(trait_list)))
+
+# find those who need to be re-run
+to_rerun = out_memory_list+time_limit_list
+to_rerun = set(to_rerun)
+len(to_rerun)
 
 
 def check_timeout(args):
