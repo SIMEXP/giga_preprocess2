b47c6661e74ad5bee0c17590ea5be5c24346f606
clarkenj <natashajayne@gmail.com>
2023-10-20 18:21:53 -0400

    removed site variable

---
+++ cobre/connectome_slurm_nosite.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH --account=rrg-pbellec
+#SBATCH --output=/lustre04/scratch/nclarke/logs/%x_%A.out
+#SBATCH --error=/lustre04/scratch/nclarke/logs/%x_%A.err
+#SBATCH --cpus-per-task=1
+
+
+GIGA_CONNECTOME_VERSION=0.4.1
+GIGA_CONNECTOME=/home/${USER}/projects/rrg-pbellec/containers/giga_connectome-${GIGA_CONNECTOME_VERSION}.simg
+FMRIPREP_DIR=/lustre04/scratch/${USER}/${DATASET}_fmriprep-20.2.7lts_1683063932/COBRE/fmriprep-20.2.7lts
+CONNECTOME_OUTPUT=/lustre04/scratch/${USER}/${DATASET}_connectomes-${GIGA_CONNECTOME_VERSION}/
+
+WORKINGDIR=${CONNECTOME_OUTPUT}/working_directory/
+
+module load apptainer/1.1.8
+
+mkdir -p $WORKINGDIR
+
+echo "${FMRIPREP_DIR}"
+if [ -d "${FMRIPREP_DIR}" ]; then
+    mkdir -p ${WORKINGDIR}
+	mkdir -p ${SLURM_TMPDIR}
+	mkdir -p ${CONNECTOME_OUTPUT}
+	echo "=========${STRATEGY}========="
+	echo "${ATLAS}"
+	apptainer run \
+		--bind ${FMRIPREP_DIR}:/data/input \
+		--bind ${SLURM_TMPDIR}:/data/output \
+		--bind ${WORKINGDIR}:/data/working \
+		${GIGA_CONNECTOME} \
+		-w /data/working \
+		--atlas ${ATLAS} \
+		--denoise-strategy ${STRATEGY} \
+		--calculate-intranetwork-average-correlation \
+		/data/input \
+		/data/output \
+		group
+	exitcode=$?  # catch exit code
+	if [ $exitcode -eq 0 ] ; then rsync -rltv --info=progress2 ${SLURM_TMPDIR}/*.h5 ${CONNECTOME_OUTPUT} ; fi
+else
+    echo "no preprocessed data for ${DATASET}"
+fi
---
+++ cobre/submit_connectomes_nosite.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --account=rrg-pbellec
+GIGA_CONNECTOME_VERSION=0.4.1
+DATASET="cobre"
+
+FMRIPREP_PATH="/lustre04/scratch/${USER}/${DATASET}_fmriprep-20.2.7lts_1683063932/COBRE/fmriprep-20.2.7lts"
+CONNECTOME=/lustre04/scratch/${USER}/${DATASET}_connectomes-${GIGA_CONNECTOME_VERSION}
+
+STRATEGIES="simple"
+ATLASES="Schaefer20187Networks MIST"
+
+for strategy in $STRATEGIES; do
+    for atlas in $ATLASES; do
+            mem=8G
+            time="6:00:00"
+
+            if [ "${atlas}" == "DiFuMo" ]; then
+                time="10:00:00"
+                mem=32G
+            fi
+
+            if [ ! -f "${CONNECTOME}/atlas-${atlas}_desc-${strategy}.h5" ]; then
+                echo "Submitting ${atlas} ${strategy}"
+                sbatch \
+                    --time=${time} --mem-per-cpu=${mem} \
+                    --job-name=${DATASET}_${atlas}_${strategy} \
+                    --export=DATASET="${DATASET}",ATLAS="${atlas}",STRATEGY="${strategy}" \
+                    ./connectome_slurm_nosite.bash
+            else
+                echo "Skipping ${atlas} ${strategy}"
+            fi
+    done
+done
