b22c63ac452bb2e7c08429930904ec1bb444c5a7
clarkenj <natashajayne@gmail.com>
2023-10-21 07:56:08 -0400

    added all atlases/strategies

--- cobre/submit_connectomes_nosite.sh
+++ cobre/submit_connectomes_nosite.sh
@@ -6,8 +6,8 @@ DATASET="cobre"
 FMRIPREP_PATH="/lustre04/scratch/${USER}/${DATASET}_fmriprep-20.2.7lts_1683063932/COBRE/fmriprep-20.2.7lts"
 CONNECTOME=/lustre04/scratch/${USER}/${DATASET}_connectomes-${GIGA_CONNECTOME_VERSION}
 
-STRATEGIES="simple"
-ATLASES="Schaefer20187Networks MIST"
+STRATEGIES="simple simple+gsr scrubbing.5 scrubbing.5+gsr scrubbing.2 scrubbing.2+gsr acompcor50"
+ATLASES="Schaefer20187Networks MIST DiFuMo"
 
 for strategy in $STRATEGIES; do
     for atlas in $ATLASES; do
