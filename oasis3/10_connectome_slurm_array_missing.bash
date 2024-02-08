0e7ff80c5d97c53421c16762da9d3dab0d01ca58
clarkenj <natashajayne@gmail.com>
2024-02-08 10:06:53 -0500

    updated preprocessing scripts

---
+++ oasis3/10_connectome_slurm_array_missing.bash
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --account=def-pbellec
+#SBATCH --output=/lustre07/scratch/nclarke/logs/oasis_conn/%x_%A.out
+#SBATCH --error=/lustre07/scratch/nclarke/logs/oasis_conn/%x_%A.out
+#SBATCH --cpus-per-task=1
+
+GIGA_CONNECTOME_VERSION=0.4.1
+GIGA_CONNECTOME="/home/nclarke/giga_connectome-${GIGA_CONNECTOME_VERSION}.simg"
+FMRIPREP_DIR="/lustre07/scratch/nclarke/oasis3_fmriprep-20.2.7lts_1689714556/oasis3_bids/fmriprep-20.2.7lts"
+CONNECTOME_OUTPUT="/lustre07/scratch/nclarke/oasis3_connectomes-${GIGA_CONNECTOME_VERSION}"
+WORKINGDIR="${CONNECTOME_OUTPUT}/working_directory"
+
+module load apptainer
+
+mkdir -p $WORKINGDIR
+
+PARTICIPANT_LABEL=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${participant_labels})
+PARTICIPANT_OUTPUT="${CONNECTOME_OUTPUT}/${PARTICIPANT_LABEL}"
+
+echo "${FMRIPREP_DIR}"
+if [ -d "${FMRIPREP_DIR}" ]; then
+    mkdir -p ${WORKINGDIR}
+        mkdir -p ${SLURM_TMPDIR}
+        mkdir -p ${PARTICIPANT_OUTPUT}
+        echo "Running ${PARTICIPANT_LABEL} connectomes"
+        echo "=========${STRATEGY}========="
+        echo "${ATLAS}"
+        apptainer run \
+                --bind ${FMRIPREP_DIR}:/data/input \
+                --bind ${SLURM_TMPDIR}:/data/output \
+                --bind ${WORKINGDIR}:/data/working \
+                ${GIGA_CONNECTOME} \
+                -w /data/working \
+                --atlas ${ATLAS} \
+                --denoise-strategy ${STRATEGY} \
+                ${INTRANETWORK_FLAG} \
+                /data/input \
+                /data/output \
+                participant \
+        --participant_label ${PARTICIPANT_LABEL}
+        exitcode=$?  # catch exit code
+        if [ $exitcode -eq 0 ] ; then rsync -rltv --info=progress2 ${SLURM_TMPDIR}/*.h5 ${PARTICIPANT_OUTPUT} ; fi
+else
+    echo "no preprocessed data for ${DATASET}"
+fi
