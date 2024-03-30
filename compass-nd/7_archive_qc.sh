1b66c08521257fef030f03749e89264794a288c7
clarkenj <natashajayne@gmail.com>
2024-03-30 11:03:20 -0400

    added qc archive script

--- compass-nd/7_archive_qc.sh
+++ compass-nd/7_archive_qc.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=qc_archive
 #SBATCH --output=/lustre04/scratch/nclarke/logs/compass-nd_qc_archive.out
 #SBATCH --error=/lustre04/scratch/nclarke/logs/compass-nd_qc_archive.err
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8G
 
 
-RAW_PATH="/lustre04/scratch/nclarke/compass-nd_giga_auto_qc-0.3.3"
+RAW_PATH="/lustre04/scratch/nclarke/compass-nd_giga_auto_qc-0.3.3_scrub.5"
 DATASET_NAME=`basename $RAW_PATH`
 
 ARCHIVE_PATH="/lustre03/nearline/6035398/giga_preprocessing_2/compass-nd_fmriprep-20.2.7lts"
