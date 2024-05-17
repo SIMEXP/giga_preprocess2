4bdd1dd103f7bb22d23053aec8bcdb1326cc73e1
clarkenj <natashajayne@gmail.com>
2024-05-17 09:00:13 -0400

    update archive for beluga

--- srpbs/18_archive_connectome.sh
+++ srpbs/18_archive_connectome.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-pbellec
 #SBATCH --job-name=conn_archive
-#SBATCH --output=/lustre07/scratch/nclarke/logs/srpbs_conn_archive.out
-#SBATCH --error=/lustre07/scratch/nclarke/logs/srpbs_conn_archive.err
+#SBATCH --output=/home/nclarke/scratch/logs/srpbs_conn_archive.out
+#SBATCH --error=/home/nclarke/scratch/logs/srpbs_conn_archive.err
 #SBATCH --time=06:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
@@ -12,10 +12,10 @@ RAW_PATH="/home/nclarke/scratch//home/nclarke/scratch/srpbs_connectome-0.4.1_MIS
 "
 DATASET_NAME=`basename $RAW_PATH`
 
-ARCHIVE_PATH="/lustre06/nearline/6035398/nclarke/${DATASET_NAME}"
+ARCHIVE_PATH="/home/nclarke/nearline/ctb-pbellec/giga_preprocessing_2/srpbs_fmriprep-20.2.7lts_1691842839/${DATASET_NAME}"
 
 mkdir -p $ARCHIVE_PATH
 
 cd ${RAW_PATH}
 echo $PWD
-tar -vczf ${ARCHIVE_PATH}/${DATASET_NAME}.tar.gz .
+tar -vcf ${ARCHIVE_PATH}/${DATASET_NAME}.tar.gz .
